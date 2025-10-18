fbdedd9304902642aeb9e07c1de759c269be5acb
deneb2 <mail@danielebroccolo.it>
2025-10-18 10:42:13 +0200

    test mqtt

--- time_logger/run.sh
+++ time_logger/run.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
+MQTT_BROKER="localhost"  # Change if the broker is on a different host
+MQTT_TOPIC="home/time_logger"
+
 while true; do
-  echo "$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+  TIMESTAMP="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
+  echo "$TIMESTAMP"
+  # Publish the timestamp to the MQTT topic
+  mosquitto_pub -h "$MQTT_BROKER" -t "$MQTT_TOPIC" -m "$TIMESTAMP"
   sleep 10
 done
