76f99cdfaa78f023c6963e09dd1685f79f354a1f
deneb2 <mail@danielebroccolo.it>
2025-10-18 19:22:43 +0200

    fix connection issues

--- time_logger/run.sh
+++ time_logger/run.sh
@@ -3,10 +3,14 @@
 MQTT_BROKER="localhost"  # Change if the broker is on a different host
 MQTT_TOPIC="home/time_logger"
 
+# Load user credentials from options
+MQTT_USER="${ADDON_MQTT_USER:-test}"
+MQTT_PASS="${ADDON_MQTT_PASS:-test}"
+
 while true; do
   TIMESTAMP="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
   echo "$TIMESTAMP"
   # Publish the timestamp to the MQTT topic
-  mosquitto_pub -h "$MQTT_BROKER" -t "$MQTT_TOPIC" -m "$TIMESTAMP"
+  mosquitto_pub -h "$MQTT_BROKER" -u "$MQTT_USER" -P "$MQTT_PASS" -t "$MQTT_TOPIC" -m "$TIMESTAMP"
   sleep 10
 done
