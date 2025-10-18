be5167e23ad5dfa38050be72a20809cceec75854
deneb2 <mail@danielebroccolo.it>
2025-10-18 19:55:36 +0200

    check parameters

--- time_logger/run.sh
+++ time_logger/run.sh
@@ -1,11 +1,16 @@
-#!/usr/bin/env bash
+#!/usr/bin/with-contenv bashio
 
 MQTT_TOPIC="home/time_logger"
 
-# Load user credentials from options
-MQTT_HOST="${ADDON_MQTT_HOST:-localhost}"
-MQTT_USER="${ADDON_MQTT_USER:-test}"
-MQTT_PASS="${ADDON_MQTT_PASS:-test}"
+# Load user credentials from options using bashio
+MQTT_HOST=$(bashio::config 'mqtt_host')
+MQTT_USER=$(bashio::config 'mqtt_user')
+MQTT_PASS=$(bashio::config 'mqtt_pass')
+
+# Optional: You can set default values if the configuration is not available
+: "${MQTT_HOST:="localhost"}"
+: "${MQTT_USER:="default_user"}"
+: "${MQTT_PASS:="default_password"}"
 
 while true; do
   TIMESTAMP="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
@@ -14,5 +19,5 @@ while true; do
   
   # Publish the timestamp to the MQTT topic
   mosquitto_pub -h "$MQTT_HOST" -u "$MQTT_USER" -P "$MQTT_PASS" -t "$MQTT_TOPIC" -m "$TIMESTAMP"
-  sleep 10
+  sleep 20
 done
